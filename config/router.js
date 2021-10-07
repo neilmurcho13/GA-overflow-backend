536ca733e543b567dfca7ae41081451d40ca01ed
Kimberley Salter <ksalter@Kimberleys-MacBook-Pro.local>
2021-10-07 17:50:41 +0100

    kim push search

--- config/router.js
+++ config/router.js
@@ -1,22 +1,24 @@
-import express from 'express'
-import blogPostController from '../controllers/blogPostController.js'
-import userController from '../controllers/userController.js'
+import express from "express";
+import blogPostController from "../controllers/blogPostController.js";
+import userController from "../controllers/userController.js";
 
-const Router = express.Router()
+const Router = express.Router();
 
 //* blogs
-Router.route('/blogs')
+Router.route("/blogs")
   .get(blogPostController.getAllBlogs)
-  .post(blogPostController.createBlog)
+  .post(blogPostController.createBlog);
 
-Router.route('/blogs/:id')
+Router.route("/blogs/:id")
   .get(blogPostController.getBlog)
   .put(blogPostController.updateBlog)
-  .delete(blogPostController.deleteBlog)
+  .delete(blogPostController.deleteBlog);
+
+Router.route("/search").get(blogPostController.searchBlogs);
 
 //* user
-Router.route('/register').post(userController.registerUser)
+Router.route("/register").post(userController.registerUser);
 
-Router.route('/login').post(userController.loginUser)
+Router.route("/login").post(userController.loginUser);
 
-export default Router
+export default Router;
