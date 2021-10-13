35733ed11f03a302372b280d436b7587e16160a3
Dominic Fergal <dominicfergal@Dominics-MacBook-Air.local>
2021-10-13 11:49:33 +0100

    expanded user model

--- models/user.js
+++ models/user.js
@@ -4,9 +4,14 @@ import mongooseHidden from 'mongoose-hidden'
 import uniqueValidator from 'mongoose-unique-validator'
 
 const user = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true, unique: true },
+  lastName: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
+  // status: {
+  //   type: String,
+  //   enum: ['student', 'alumni', 'instructor']
+  // }
 })
 
 user.pre('save', function (next) {
