52ca6716c8957590ff88871556d54132e872be07
Dominic Jonas <ppkk.dominic@gmail.com>
2021-11-25 21:18:39 +0000

    msg

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
 export const dbUri =
   process.env.DB_URI || 'mongodb://localhost/ga-overflow'
-export const port = process.env.PORT || 3000
+// export const port = process.env.PORT || 3000
 export const secret = process.env.SECRET || 'secret'
